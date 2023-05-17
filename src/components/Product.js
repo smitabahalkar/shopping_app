c0ced9022be2aca33735fabe4d703d0ae764c1e3
smitabahalkar <smitabahalkar@users.github.com>
2023-05-16 20:21:41 -0500

    poduct dashboard basic display

---
+++ src/components/Product.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+const Product = () => {
+  const [products, getProducts] = useState([]);
+  //api
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => getProducts(result));
+  }, []);
+
+  const cards = products.map((product) => (
+    <div className="col-md-3">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  ));
+
+  return (
+    <>
+      <h1>Product Dashboard</h1>
+      <div className="row">{cards}</div>
+    </>
+  );
+};
+
+export default Product;
